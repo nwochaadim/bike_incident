63519f865c3f9d26c691da8cfb570c59fa6b7ccf
Nwocha Adim <adimoranma.felix@andela.com>
2018-03-01 06:30:12 +0100

    WIP fix spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get "/", to: "home#welcome"
+      root to: "home#welcome"
     end
   end
 
--- spec/requests/visitor_visits_home_spec.rb
+++ spec/requests/visitor_visits_home_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "visitor visits home" do
   describe "GET /api/v1/" do
     it "sends a welcome message to the user" do
-      get api_v1_path
+      get api_v1_root_path
 
       expect(json_parsed_body["message"]).to match /Welcome/
     end
